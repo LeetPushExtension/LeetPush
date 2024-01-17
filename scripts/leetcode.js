06e04e8c6975330e3fd650284aa8cf327fda1912
husamahmud <husamahmud@gmail.com>
2024-01-18 00:45:40 +0300

    [refactor] Remove unused file

--- scripts/leetcode.js
+++
@@ -1,6 +0,0 @@
-if (window.location.href.startsWith("https://leetcode.com/problems/")) {
-    const anchor = document.querySelector("div.flex.w-full.flex-1.flex-col.gap-4.overflow-y-auto.px-4.py-5 > div.flex.items-start.justify-between.gap-4 > div.flex.items-start.gap-2 > div > a");
-    if (anchor) {
-        console.log(anchor.innerText);
-    }
-}
