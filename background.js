4bf9dfccebf92ce93894a72496cc5e0d0a83bf72
husamahmud <husamahmud@gmail.com>
2024-01-17 16:07:33 +0300

    [feat] Add `onClicked` event

--- background.js
+++ background.js
@@ -2,16 +2,18 @@ chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
     if (changeInfo.status === 'complete') {
         chrome.scripting.executeScript({
             target: {tabId: tabId}, func: () => {
+                let probName, solutionText, fileExtension;
                 /** Problem Name */
-                const problemNameAnchor = document.querySelector("div.flex.items-start.justify-between.gap-4 > div.flex.items-start.gap-2 > div > a");
-                if (problemNameAnchor) {
-                    const probName = problemNameAnchor.innerText.replace(".", "").replaceAll(" ", "-");
-                    console.log(probName);
+                const problemNameElement = document.querySelector("div.flex.items-start.justify-between.gap-4 > div.flex.items-start.gap-2 > div > a");
+                if (problemNameElement) {
+                    probName = problemNameElement.innerText.replace(".", "").replaceAll(" ", "-");
+                    sessionStorage.setItem('probName', probName);
                 }
                 /** Solution */
                 const solutionElement = document.querySelector("[class^=\"language-\"]");
                 if (solutionElement) {
-                    console.log(solutionElement.innerText);
+                    solutionText = solutionElement.innerText;
+                    sessionStorage.setItem('solutionText', solutionText);
                 }
                 /** File Extension */
                 const solutionLanguageText = document.querySelector("div.flex.h-full.w-full.flex-col.overflow-hidden.rounded > div > div > div.w-full.flex-1.overflow-y-auto > div > div.bg-fill-quaternary.dark\\:bg-fill-quaternary.relative.w-full.overflow-hidden.rounded-lg > div > div.text-caption.px-4.py-2.font-medium.text-text-primary.dark\\:text-text-primary")
@@ -37,10 +39,25 @@ chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
                     "Elixir": ".ex",
                 };
                 if (solutionLanguageText) {
-                    const fileExtension = fileExtensions[solutionLanguageText.innerText]
-                    console.log(fileExtension)
+                    fileExtension = fileExtensions[solutionLanguageText.innerText]
+                    sessionStorage.setItem('fileExtension', fileExtension);
                 }
             }
         });
     }
+});
+chrome.action.onClicked.addListener(tab => {
+    chrome.scripting.executeScript({
+        target: {tabId: tab.id},
+        func: () => {
+            const probName = sessionStorage.getItem('probName');
+            const solutionText = sessionStorage.getItem('solutionText');
+            const fileExtension = sessionStorage.getItem('fileExtension');
+            if (window.location.href.includes('submissions')) {
+                console.log('We did it')
+                console.log(`${probName}${fileExtension}`)
+                console.log(solutionText)
+            }
+        }
+    })
 });
