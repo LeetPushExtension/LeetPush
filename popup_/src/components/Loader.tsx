476adb1c28bb0c3a402cada65796a895b98bbc69
husamahmud <husamahmud@gmail.com>
2024-08-14 17:52:00 +0300

    DELETE

--- popup_/src/components/Loader.tsx
+++
@@ -1,5 +0,0 @@
-export default function Loader() {
-  return (
-    <div className="loader w-full mx-auto mt-8 mb-5"></div>
-  )
-}
--- popup_/src/vite-env.d.ts
+++
@@ -1,7 +0,0 @@
-interface ImportMetaEnv {
-  readonly VITE_LEETPUSH_API_URL: string;
-}
-
-interface ImportMeta {
-  readonly env: ImportMetaEnv;
-}
